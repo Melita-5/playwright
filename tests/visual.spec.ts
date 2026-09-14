71fc64bdaf08d70d3961bef6ed8a6e0e4fc70376
Melita-5 <art2designculture@gmail.com>
2026-09-14 13:36:06 +0200

    Add GitHub Actions workflow for Playwright tests

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -5,34 +5,34 @@ const BASE = 'https://practicesoftwaretesting.com';
 test.describe('Visual checks', () => {
 
 // Green tests are commented out because they are not working in the current version of Playwright. They will be fixed in a future version.
-//   test('login page looks unchanged', async ({ page }) => {
-//     await page.goto(`${BASE}/auth/login`);
-//     await expect(page).toHaveScreenshot('login-baseline.png', {
-//       animations: 'disabled',
-//     });
-//   });
+  test('login page looks unchanged', async ({ page }) => {
+    await page.goto(`${BASE}/auth/login`);
+    await expect(page).toHaveScreenshot('login-baseline.png', {
+      animations: 'disabled',
+    });
+  });
 
-//   test('home page header looks unchanged', async ({ page }) => {
-//     await page.goto(`${BASE}/`);
-//     await expect(page.locator('nav').first()).toHaveScreenshot('header-baseline.png', {
-//       animations: 'disabled',
-//     });
-//   });
+  test('home page header looks unchanged', async ({ page }) => {
+    await page.goto(`${BASE}/`);
+    await expect(page.locator('nav').first()).toHaveScreenshot('header-baseline.png', {
+      animations: 'disabled',
+    });
+  });
 
 // Change image so the test will fail. This is to demonstrate how visual testing works.
-test('login page looks unchanged', async ({ page }) => {
-  await page.goto(`${BASE}/auth/login`);
-  await page.addStyleTag({ content: '.btnSubmit { margin-top: 50px; }' });   // ← added
-  await expect(page).toHaveScreenshot('login-baseline.png', {
-    animations: 'disabled',
-  });
-});
+// test('login page looks unchanged', async ({ page }) => {
+//   await page.goto(`${BASE}/auth/login`);
+//   await page.addStyleTag({ content: '.btnSubmit { margin-top: 50px; }' });   // ← added
+//   await expect(page).toHaveScreenshot('login-baseline.png', {
+//     animations: 'disabled',
+//   });
+// });
 
-test('home page header looks unchanged', async ({ page }) => {
-  await page.goto(`${BASE}/`);
-  await page.addStyleTag({ content: '.nav-link { font-size: 22px; }' });     // ← added
-  await expect(page.locator('nav').first()).toHaveScreenshot('header-baseline.png', {
-    animations: 'disabled',
-  });
-});
+// test('home page header looks unchanged', async ({ page }) => {
+//   await page.goto(`${BASE}/`);
+//   await page.addStyleTag({ content: '.nav-link { font-size: 22px; }' });     // ← added
+//   await expect(page.locator('nav').first()).toHaveScreenshot('header-baseline.png', {
+//     animations: 'disabled',
+//   });
+// });
 });
